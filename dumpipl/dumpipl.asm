811cf7a046b7f22f652997742bb1433666ce6a28
up-n-atom <adam.jaremko@gmail.com>
2021-03-12 12:33:44 -0500

    dumpipl: Cleanup

--- dumpipl/dumpipl.asm
+++ dumpipl/dumpipl.asm
@@ -30,27 +30,29 @@ start:
 	or al, 0xe0
 	out SERIAL_STATUS, al
 					; Dump IPL based on system
+					; WonderSwan Color/SwanCrystal
 	mov bx, 0xfe00
 	mov cx, 0x2000
 	in al, SYSTEM_CTRL1
 	test al, 2
-	jnz .fuelload
+	jnz .load
+					; WonderSwan @ 0xff00/0x1000
 	shr cx, 1
 	or bh, 1
-.fuelload:
+.load:
 	mov ds, bx
-.dumpload:
+.dump:
 	in al, SERIAL_STATUS
 	test al, 4
-	jz .dumpload
+	jz .dump
 	lodsb
 	out SERIAL_DATA, al
-	loop .dumpload
-					; Disable Serial
-.junkinthetrunk:
+	loop .dump
+					; Flush and disable serial
+.flush:
 	in al, SERIAL_STATUS
 	test al, 4
-	jz .junkinthetrunk
+	jz .flush
 	and al, 0x5f
 	out SERIAL_STATUS, al
 					; Power down
